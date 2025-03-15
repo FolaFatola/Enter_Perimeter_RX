93af260bee75d68caf13fbee6970dd696a78d84f
Fola Fatola <ffatola@uwaterloo.ca>
2025-03-14 23:24:14 -0400

    Init RTC driver

---
+++ Security/Inc/rtc.hpp
@@ -0,0 +1,19 @@
+/*
+ * rtc.hpp
+ *
+ *  Created on: Mar 14, 2025
+ *      Author: folafatola
+ */
+
+#ifndef SECURITY_INC_RTC_HPP_
+#define SECURITY_INC_RTC_HPP_
+
+
+
+
+
+
+
+
+
+#endif /* SECURITY_INC_RTC_HPP_ */
---
+++ Security/Src/rtc.cpp
@@ -0,0 +1,8 @@
+/*
+ * rtc.cpp
+ *
+ *  Created on: Mar 14, 2025
+ *      Author: folafatola
+ */
+
+#include "rtc.hpp"
