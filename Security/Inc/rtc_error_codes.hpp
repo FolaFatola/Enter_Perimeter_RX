e80b9740a7d8134b7fc11fa8386f313202a668ff
Fola Fatola <ffatola@uwaterloo.ca>
2025-03-16 01:59:19 -0400

    Implented initial RTC error codes

---
+++ Security/Inc/rtc_error_codes.hpp
@@ -0,0 +1,21 @@
+/*
+ * rtc_error_codes.hpp
+ *
+ *  Created on: Mar 15, 2025
+ *      Author: folafatola
+ */
+
+#ifndef INC_RTC_ERROR_CODES_HPP_
+#define INC_RTC_ERROR_CODES_HPP_
+
+typedef enum RTC_Status_E {
+	RTC_OK,
+	RTC_INIT_FAIL,
+	RTC_RECV_FAI
+} RTC_Status_E;
+
+
+
+
+
+#endif /* INC_RTC_ERROR_CODES_HPP_ */
