87807177c7560412da4546b070a24f592fdfab28
Fola Fatola <ffatola@uwaterloo.ca>
2025-04-12 21:49:13 -0400

    nrf2401 write to and read from config register

--- F401RE_RX/Core/Src/main.cpp
+++ F401RE_RX/Core/Src/main.cpp
@@ -23,6 +23,7 @@
 /* USER CODE BEGIN Includes */
 #include <cstdio>
 #include "rtc.hpp"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -32,6 +33,9 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define WRITE_CONFIG_REG 0x20
+#define READ_CONFIG_REG 0x00
+#define PWR_UP_BIT (1 << 1)
 
 /* USER CODE END PD */
 
@@ -42,6 +46,8 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c2;
+
+SPI_HandleTypeDef hspi1;
 
 UART_HandleTypeDef huart2;
 
@@ -54,6 +60,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C2_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -94,6 +101,7 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C2_Init();
   MX_USART2_UART_Init();
+  MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
   bool use_tfh_time = true;
   uint8_t seconds = 2;
@@ -108,23 +116,47 @@ int main(void)
   rtc_sensor_time.rtc_init();
 
   char message[100];
+
+  //when due to voltage being above 1.9V, we are currently in power down mode.
+
+  //PB6 is the CS PIN. PC7 is CE pin, and PA9 is for the external interrupt.
+  //Set the PWR_UP bit in the config register.
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+  uint8_t command = WRITE_CONFIG_REG;
+  uint8_t config_reg_bits = PWR_UP_BIT;
+  HAL_SPI_Transmit(&hspi1, &command, 1, 100);
+  HAL_SPI_Transmit(&hspi1, &config_reg_bits, 1, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+
+  //Read back the PWR_UP bit in the config register.
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+  command = READ_CONFIG_REG;
+  uint8_t read_config_bits = 0;
+  HAL_SPI_Transmit(&hspi1, &command, 1, 100);
+  HAL_SPI_Receive(&hspi1, &read_config_bits, 1, 100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+  HAL_Delay(3);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  rtc_sensor_time.rtc_get_time_unit(SECONDS, seconds);
-	  rtc_sensor_time.rtc_get_time_unit(MINUTES, minutes);
-	  rtc_sensor_time.rtc_get_time_unit(HOURS, hours);
-	  rtc_sensor_time.rtc_get_time_unit(WEEK_DAY, weekday);
-	  rtc_sensor_time.rtc_get_time_unit(DATE_DAY, date_day);
-	  rtc_sensor_time.rtc_get_time_unit(MONTH, month);
-	  rtc_sensor_time.rtc_get_time_unit(YEAR, year);
+//	  rtc_sensor_time.rtc_get_time_unit(SECONDS, seconds);
+//	  rtc_sensor_time.rtc_get_time_unit(MINUTES, minutes);
+//	  rtc_sensor_time.rtc_get_time_unit(HOURS, hours);
+//	  rtc_sensor_time.rtc_get_time_unit(WEEK_DAY, weekday);
+//	  rtc_sensor_time.rtc_get_time_unit(DATE_DAY, date_day);
+//	  rtc_sensor_time.rtc_get_time_unit(MONTH, month);
+//	  rtc_sensor_time.rtc_get_time_unit(YEAR, year);
+//
+//	  HAL_UART_Transmit(&huart2, (uint8_t *)message, sprintf(message, "Seconds: %d, Minutes: %d, Hours: %d, "
+//			  "Week_Day: %d, Date_Day %d, Month %d, Year %d\r\n",
+//			  seconds, minutes, hours, weekday, date_day, month, year+millennium), 100);
 
-	  HAL_UART_Transmit(&huart2, (uint8_t *)message, sprintf(message, "Seconds: %d, Minutes: %d, Hours: %d, "
-			  "Week_Day: %d, Date_Day %d, Month %d, Year %d\r\n",
-			  seconds, minutes, hours, weekday, date_day, month, year+millennium), 100);
+	  HAL_UART_Transmit(&huart2, (uint8_t *)message, sprintf(message, "The config bit status is %d\r\n",
+			  read_config_bits), 100);
 
 	  HAL_Delay(1000);
     /* USER CODE END WHILE */
@@ -210,6 +242,44 @@ static void MX_I2C2_Init(void)
 }
 
 /**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -249,12 +319,36 @@ static void MX_USART2_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
